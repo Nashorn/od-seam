83135bcef47af9e8ba8ea28d18bcb83ee3e6b11c
JS <jaysmith024@gmail.com>
2024-08-21 10:09:51 -0400

    package bump; updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const Config = require('./-appconfig.js');
+
 const BUILDCONFIG = require('../../-buildconfig.js');//5/25/20 -- loads from root project
 // const test = require(__dirname + "/test.js");
 const fs = require('fs');
